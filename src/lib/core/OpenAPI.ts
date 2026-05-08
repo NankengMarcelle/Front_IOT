80bb6243880be0311e16d9df46d8e1694139c4c1
NguetchuissiBrunel <landrybrunel5@gmail.com>
2026-05-08 13:14:34 +0100

    feat: initialize OpenAPI configuration with base URL and token resolver

--- src/lib/core/OpenAPI.ts
+++ src/lib/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://iotsoilbackend-production.up.railway.app',
+    BASE: 'https://smart-agro.duckdns.org',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
